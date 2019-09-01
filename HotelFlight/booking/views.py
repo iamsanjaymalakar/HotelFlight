d8b87cbd1d89fa9dc5a8c6fc8fe670af9ce9bae0
iamsanjaymalakar <iamsanjaymalakar@gmail.com>
2019-09-01 22:32:58 +0600

    Modified Booking SubSystem 1. Changed UI 2. Added DatePicker for searching 3. ImageView of room 4. New price display

--- HotelFlight/booking/views.py
+++ HotelFlight/booking/views.py
@@ -1,3 +1,4 @@
+from database.models import *
 from search.forms import SearchHotelForm, SearchFlightForm
 from django.shortcuts import render
 from .forms import HotelBookingForm, FlightBookingForm
@@ -27,12 +28,13 @@ def hotelbookingpage(request):
     roomCount = request.GET.get('room', '')
     dateCount = daysBetween(checkIn, checkOut)
     hotelRoomID = request.GET.get('hrid', '')
+    adultCount = request.GET.get('adult', '')
     cursor = connection.cursor()
-    cursor.execute("SELECT R.RoomType, R.SingleBedCount, R.DoubleBedCount,HR.Room_id as 'roomID',"
-                   "HR.Price*%s*%s as 'Price',R.AirConditioner,HR.Complimentary_Breakfast,HR.wifi FROM "
+    cursor.execute("SELECT R.RoomType, R.SingleBedCount, R.DoubleBedCount,HR.Room_id as 'roomID',HR.Price*%s as "
+                   "'NPrice',HR.Price*%s*%s as 'Price',R.AirConditioner,HR.Complimentary_Breakfast,HR.wifi FROM "
                    "database_hotel_room HR JOIN database_room R ON HR.Room_id=R.id "
                    "WHERE HR.id=%s",
-                   [int(roomCount), dateCount, int(hotelRoomID)])
+                   [int(roomCount), int(roomCount), dateCount, int(hotelRoomID)])
     room = namedtuplefetchall(cursor)
     cursor.execute("SELECT H.Hotel_Name, H.Hotel_Location, H.Hotel_Country,H.Address,H.CompanyAdmin_id as 'uid' FROM "
                    "database_hotel_room HR JOIN database_hotel H ON HR.Hotel_id=H.id WHERE HR.id=%s",
@@ -41,127 +43,92 @@ def hotelbookingpage(request):
     form = HotelBookingForm()
     hotelForm = SearchHotelForm()
     flightForm = SearchFlightForm()
+    # setting initial values for room and adult
+    hotelForm.fields['room'].initial = int(roomCount)
+    hotelForm.fields['adult'].initial = int(adultCount)
     return render(request, "booking/hotelbooking.html",
-                  {'room': room[0], 'hotel': hotel[0], 'form': form, 'hotelForm': hotelForm, 'flightForm': flightForm})
+                  {'room': room[0], 'hotel': hotel[0], 'form': form, 'hotelForm': hotelForm, 'flightForm': flightForm,
+                   'daysCount': dateCount})
 
 
 def hotelbookingpaymet(request):
-    roomcount = request.GET.get('room', '')
-    hrid = request.GET.get('hrid', '')
-    hrid = int(hrid)
-    roomcount = int(roomcount)
+    checkIn = request.GET.get('checkin', '')
+    checkOut = request.GET.get('checkout', '')
+    roomCount = request.GET.get('room', '')
+    dateCount = daysBetween(checkIn, checkOut)
+    hotelRoomID = request.GET.get('hrid', '')
+    adultCount = request.GET.get('adult', '')
     cursor = connection.cursor()
-    cursor.execute("SELECT R.RoomType, R.SingleBedCount, R.DoubleBedCount,HR.Room_id as 'roomID',"
-                   "HR.Price*%s as 'Price',R.AirConditioner,HR.Complimentary_Breakfast,HR.wifi FROM "
+    cursor.execute("SELECT R.RoomType, R.SingleBedCount, R.DoubleBedCount,HR.Room_id as 'roomID',HR.Price*%s as "
+                   "'NPrice',HR.Price*%s*%s as 'Price',R.AirConditioner,HR.Complimentary_Breakfast,HR.wifi FROM "
                    "database_hotel_room HR JOIN database_room R ON HR.Room_id=R.id "
                    "WHERE HR.id=%s",
-                   [roomcount, hrid])
+                   [int(roomCount), int(roomCount), dateCount, int(hotelRoomID)])
     room = namedtuplefetchall(cursor)
     cursor.execute("SELECT H.Hotel_Name, H.Hotel_Location, H.Hotel_Country,H.Address,H.CompanyAdmin_id as 'uid' FROM "
                    "database_hotel_room HR JOIN database_hotel H ON HR.Hotel_id=H.id WHERE HR.id=%s",
-                   [hrid])
+                   [int(hotelRoomID)])
     hotel = namedtuplefetchall(cursor)
     form = HotelBookingForm()
-    hotelform = SearchHotelForm()
-    flightform = SearchFlightForm()
+    hotelForm = SearchHotelForm()
+    flightForm = SearchFlightForm()
+    # setting initial values for room and adult
+    hotelForm.fields['room'].initial = int(roomCount)
+    hotelForm.fields['adult'].initial = int(adultCount)
     return render(request, "booking/bookingpayment.html",
-                  {'room': room[0], 'hotel': hotel[0], 'form': form, 'hotelform': hotelform, 'flightform': flightform})
+                  {'room': room[0], 'hotel': hotel[0], 'form': form, 'hotelForm': hotelForm, 'flightForm': flightForm,
+                   'daysCount': dateCount})
 
 
 def BookingConfirmationPage(request):
-    # mine
-    roomcount = request.GET.get('room', '')
-    hrid = request.GET.get('hrid', '')
-    hrid = int(hrid)
-    roomcount = int(roomcount)
-    print('HRID')
-    print(hrid)
-    print('Room ID')
-    print(roomcount)
+    checkIn = request.GET.get('checkin', '')
+    checkOut = request.GET.get('checkout', '')
+    roomCount = request.GET.get('room', '')
+    dateCount = daysBetween(checkIn, checkOut)
+    hotelRoomID = request.GET.get('hrid', '')
+    adultCount = request.GET.get('adult', '')
     cursor = connection.cursor()
-    cursor.execute("SELECT R.RoomType, R.SingleBedCount, R.DoubleBedCount,HR.Room_id as 'roomID',"
-                   "HR.Price*%s as 'Price',R.AirConditioner,HR.Complimentary_Breakfast,HR.wifi FROM "
+    cursor.execute("SELECT R.RoomType, R.SingleBedCount, R.DoubleBedCount,HR.Room_id as 'roomID',HR.Price*%s as "
+                   "'NPrice',HR.Price*%s*%s as 'Price',R.AirConditioner,HR.Complimentary_Breakfast,HR.wifi FROM "
                    "database_hotel_room HR JOIN database_room R ON HR.Room_id=R.id "
                    "WHERE HR.id=%s",
-                   [roomcount, hrid])
+                   [int(roomCount), int(roomCount), dateCount, int(hotelRoomID)])
     room = namedtuplefetchall(cursor)
-    print(room[0])
     cursor.execute("SELECT H.Hotel_Name, H.Hotel_Location, H.Hotel_Country,H.Address,H.CompanyAdmin_id as 'uid' FROM "
                    "database_hotel_room HR JOIN database_hotel H ON HR.Hotel_id=H.id WHERE HR.id=%s",
-                   [hrid])
+                   [int(hotelRoomID)])
     hotel = namedtuplefetchall(cursor)
     form = HotelBookingForm()
-    hotelform = SearchHotelForm()
-    flightform = SearchFlightForm()
-    # mine
-
-    FirstName = request.GET.get('first_name', '')
-    LastName = request.GET.get('last_name', '')
-    Email = request.GET.get('email', '')
-    Phone = request.GET.get('phone', '')
-    dest = request.GET.get('hoteldest', '')
-    checkin = request.GET.get('checkin', '')
-    checkout = request.GET.get('checkout', '')
-    roomcount = request.GET.get('room', '')
-    adultcount = request.GET.get('adult', '')
-    hid = request.GET.get('hid', '')
-    huid = request.GET.get('huid', '')
-    hrid = request.GET.get('hrid', '')
-    uid = request.GET.get('uid', '')
-
-    hrid = int(hrid)
-    uid = int(uid)
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT MAX(id) from database_booking")
-    results = cursor.fetchone()
-    id = 1
-    if results[0] is None:
-        id = 1
-    else:
-        id = results[0] + 1
-    cursor.execute("SELECT Price FROM database_hotel_room where id=%s", [hrid])
-    results = cursor.fetchone()
-    Price = results[0]
-    cursor.execute("SELECT Percentage FROM database_hotel where id=%s", [hid])
-    results = cursor.fetchone()
-    rooms = int(roomcount)
-    date_format = "%Y-%m-%d"
-    a = datetime.strptime(checkout, date_format)
-    b = datetime.strptime(checkin, date_format)
-    delta = a - b
-    Percentage = results[0] / 100
-    PaidMoney = Percentage * Price * rooms * delta.days
-    MoneyToPay = Price * (1 - Percentage) * rooms * delta.days
-    # ei percentage should come from cancellation policy\
-    cursor.execute(
-        "SELECT Percentage_refunding FROM database_cancellation_policy WHERE id =(SELECT Cancellation_Policy_id FROM database_hotel_room where id=%s)",
-        [hrid])
-    results = cursor.fetchone()
-    Percentage_refunding = results[0] / 100
-    MoneyToRefund = PaidMoney * Percentage_refunding
-    cursor.execute(
-        "INSERT INTO database_booking (id, MoneyToPay, MoneyToRefund, DateOfBooking, DateOfCancellation, User_id,PaidMoney,isCancelled,Status) "
-        "VALUES (%s, %s, %s, CURRENT_DATE, CURRENT_DATE, %s, %s,0,0)", [id, MoneyToPay, MoneyToRefund, uid, PaidMoney])
-    cursor.execute("SELECT MAX(id) from database_hotel_booking")
-    results = cursor.fetchone()
-    new_id = 1
-    if results[0] is None:
-        new_id = 1
-    else:
-        new_id = results[0] + 1
-    cursor.execute(
-        "INSERT INTO database_hotel_booking (id, Booking_id, Hotel_Room_id, Checkin_Date, Checkout_Date,TotalRooms,isApproved) "
-        "VALUES (%s,%s,%s,%s,%s,%s,0)", [new_id, id, hrid, checkin, checkout, roomcount])
-    cursor.execute("UPDATE database_hotel_room "
-                   "SET FreeRoomCount = FreeRoomCount - %s "
-                   "WHERE id=%s", [roomcount, hrid])
-
+    hotelForm = SearchHotelForm()
+    flightForm = SearchFlightForm()
+    firstName = request.GET.get('first_name', '')
+    lastName = request.GET.get('last_name', '')
+    email = request.GET.get('email', '')
+    hotelID = request.GET.get('hid', '')
+    userID = request.GET.get('uid', '')
+    price = Hotel_Room.objects.get(id=int(hotelRoomID)).Price
+    percentage = Hotel.objects.get(id=int(hotelID)).Percentage / 100
+    paidMoney = percentage * price * int(roomCount) * dateCount
+    moneyToPay = price * (1 - percentage) * int(roomCount) * dateCount
+    percentageRefunding = Cancellation_Policy.objects.get(
+        id=Hotel_Room.objects.get(id=int(hotelRoomID)).Cancellation_Policy_id).Percentage_refunding / 100
+    moneyToRefund = paidMoney * percentageRefunding
+    today = datetime.today().strftime('%Y-%m-%d')
+    bookingObject = Booking(MoneyToPay=moneyToPay, MoneyToRefund=moneyToRefund, DateOfBooking=today,
+                            DateOfCancellation=today, User_id=userID, PaidMoney=paidMoney, isCancelled=False,
+                            Status=False)
+    bookingObject.save()
+    hotelBookingObject = Hotel_Booking(Booking=bookingObject, Hotel_Room_id=hotelRoomID, Checkin_Date=checkIn,
+                                       Checkout_Date=checkOut, TotalRooms=roomCount, isApproved=False)
+    hotelBookingObject.save()
+    hotelRoomObject = Hotel_Room.objects.get(id=hotelRoomID)
+    hotelRoomObject.FreeRoomCount -= int(roomCount)
+    hotelRoomObject.save()
+    '''
     # generate confirmation mail
     # create message object instance
     print(FirstName)
     print(LastName)
-
     p = canvas.Canvas('./booking/pdfs/ex.pdf')
     p.drawString(250, 700, "Itinerary")
     Fname = "First Name: " + FirstName
@@ -172,64 +139,47 @@ def BookingConfirmationPage(request):
     OutDate = "Check Out: " + checkout
     BookAmount = "Booking Amount: " + str(PaidMoney)
     RestAmount = "More to Pay: " + str(MoneyToPay)
-
     p.drawString(100, 620, "Personal Information")
     p.drawString(100, 600, Fname)
     p.drawString(100, 580, Lname)
     p.drawString(100, 560, Contact)
-
     p.drawString(100, 500, "Booking Information")
     p.drawString(100, 480, HotelName)
     p.drawString(100, 460, InDate)
     p.drawString(100, 440, OutDate)
     p.drawString(100, 420, BookAmount)
     p.drawString(100, 400, RestAmount)
-
     p.drawString(100, 350, "This booking has been confirmed.")
-
     p.drawImage('./HotelFlight/static/booking/barcode.jpg', 400, 750, 6 * cm, 2 * cm)
     p.showPage()
     p.save()
-
     # create message object instance
-    '''msg = MIMEMultipart()
-
+    msg = MIMEMultipart()
     message = "Your booking has been confirmed! Thanks for staying with us!!"
-
     # setup the parameters of the message
     password = "proxierOSP"
     msg['From'] = "onlineserviceprovider131625@gmail.com"
     msg['To'] = Email
     msg['Subject'] = "Response to Hotel/Flight Booking"
-
     # add in the message body
     msg.attach(MIMEText(message, 'plain'))
-
     filename = "ex.pdf"
     attachment = open("./booking/pdfs/ex.pdf", "rb")
     p = MIMEBase('application', 'octet-stream')
     p.set_payload((attachment).read())
     encoders.encode_base64(p)
-
     p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
     msg.attach(p)
     # create server
     server = smtplib.SMTP('smtp.gmail.com: 587')
-
     server.starttls()
-
     # Login Credentials for sending the mail
     # server.login(msg['From'], password)
-
     # send the message via the server.
     # server.sendmail(msg['From'], msg['To'], msg.as_string())
-
     # server.quit()
-
     print("email sent")
-
     msg = MIMEMultipart()
-
     message = "Your reservation has been succesfully confirmed!  " \
               "" \
               "" \
@@ -242,33 +192,30 @@ def BookingConfirmationPage(request):
     msg['From'] = "onlineserviceprovider131625@gmail.com"
     msg['To'] = Email
     msg['Subject'] = "Hotel reservation on" + hotel[0].Hotel_Name + " hotel"
-
     # add in the message body
     msg.attach(MIMEText(message, 'plain'))
-
     # filename = "query1.txt"
     # attachment = open("./ReservationProject_Root/query1.txt", "rb")
     # p = MIMEBase('application', 'octet-stream')
     # p.set_payload((attachment).read())
     # encoders.encode_base64(p)
-
     # p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
     # msg.attach(p)
     # create server
     #server = smtplib.SMTP('smtp.gmail.com: 587')
-
     #server.starttls()
-
     # Login Credentials for sending the mail
     #server.login(msg['From'], password)
-
     # send the message via the server.
     #server.sendmail(msg['From'], msg['To'], msg.as_string())
-
     #server.quit()
     #print("email sent")'''
+    # setting initial values for room and adult
+    hotelForm.fields['room'].initial = int(roomCount)
+    hotelForm.fields['adult'].initial = int(adultCount)
     return render(request, "booking/bookingconfirmation.html",
-                  {'room': room[0], 'hotel': hotel[0], 'form': form, 'hotelform': hotelform, 'flightform': flightform})
+                  {'room': room[0], 'hotel': hotel[0], 'form': form, 'hotelForm': hotelForm, 'flightForm': flightForm,
+                   'daysCount': dateCount})
 
 
 def FlightBookingPage(request):
